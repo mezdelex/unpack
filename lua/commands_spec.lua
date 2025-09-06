b8c55954cd433368c8e4a14cbc1222959a938a96
Mezdelex <59997405+mezdelex@users.noreply.github.com>
2025-09-06 17:30:03 +0200

    chore(cleanup): remove stale test from the initial commit (#3)
    * chore(tests): first attempt to integrate ci with neovim
    
    * chore(ci): changed ci name to reflect what it's doing
    
    * test(commands_spec): added commands_spec test; testing ci
    
    * test(busted): make busted command not silent
    
    * test(busted): add the bang (!) back to tell headless neovim that the command is a shell command not an editor interpreted command
    
    * fix(test): luarocks --local installation PATH
    
    * test(busted): remove the --lua-version=5.1 flag
    
    * test(lua_module): export LUA_PATH
    
    * test(lua_modules): add lua modules to the LUA_PATH as well
    
    * fix(commands_spec): change require call directly to luassert
    
    * fix(LUA_PATH): add current directory lua files to LUA_PATH to be discoverable
    
    * fix(vim): added vim global variable
    
    * fix(test,dependencies): set dependencies properly
    
    * test(commands_spec): add global stubs
    
    * fix(mocks): added several missing mocks
    
    * fix(tests): several fixes...
    
    * test(pull,update,build): working pull, update and build methods, pending load and clean
    
    * test(commands): added commands coverage
    
    * test(commands_spec): moved commands_spec to the proper test directory and added fixtures.lua to contain required fixtures
    
    * chore(cleanup): remove stale spec from the initial commit

--- lua/commands_spec.lua
+++
@@ -1,8 +0,0 @@
-local inspect = require("inspect")
-local assert = require("luarocks.busted.assert")
-
-describe("commands", function()
-	it("should be true", function()
-		assert.True(true)
-	end)
-end)
