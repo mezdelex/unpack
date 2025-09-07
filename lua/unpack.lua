22330a5f7d2990370be812d501b53b65063eb64a
mezdelex <alexcondegomez@gmail.com>
2025-09-07 14:01:37 +0200

    style(stylua): format properly so the CI should not fail now

--- lua/unpack.lua
+++ lua/unpack.lua
@@ -7,38 +7,38 @@ local M = {} ---@class UnPack
 
 ---@param opts? UnPack.Config.UserOpts
 function M.setup(opts)
-    local commands = require("commands")
-    local config = require("config")
-    local group = "UnPack"
-    require("extensions")
+	local commands = require("commands")
+	local config = require("config")
+	local group = "UnPack"
+	require("extensions")
 
-    config.setup(opts)
+	config.setup(opts)
 
-    vim.api.nvim_create_augroup(group, { clear = true })
+	vim.api.nvim_create_augroup(group, { clear = true })
 
-    vim.api.nvim_create_autocmd("PackChanged", {
-        callback = function(args)
-            local kind = args.data.kind ---@type string
+	vim.api.nvim_create_autocmd("PackChanged", {
+		callback = function(args)
+			local kind = args.data.kind ---@type string
 
-            if kind == "install" or kind == "update" then
-                local spec = args.data.spec ---@type UnPack.Spec
+			if kind == "install" or kind == "update" then
+				local spec = args.data.spec ---@type UnPack.Spec
 
-                commands.build({ spec })
-            end
-        end,
-        group = group,
-    })
+				commands.build({ spec })
+			end
+		end,
+		group = group,
+	})
 
-    vim.api.nvim_create_user_command("PackBuild", commands.build, {})
-    vim.api.nvim_create_user_command("PackClean", commands.clean, {})
-    vim.api.nvim_create_user_command("PackLoad", commands.load, {})
-    vim.api.nvim_create_user_command("PackPull", commands.pull, {})
-    vim.api.nvim_create_user_command("PackUpdate", commands.update, {})
+	vim.api.nvim_create_user_command("PackBuild", commands.build, {})
+	vim.api.nvim_create_user_command("PackClean", commands.clean, {})
+	vim.api.nvim_create_user_command("PackLoad", commands.load, {})
+	vim.api.nvim_create_user_command("PackPull", commands.pull, {})
+	vim.api.nvim_create_user_command("PackUpdate", commands.update, {})
 
-    commands.load()
-    commands.pull()
+	commands.load()
+	commands.pull()
 
-    M.commands = commands
+	M.commands = commands
 end
 
 return M
